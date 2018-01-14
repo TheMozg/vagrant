fa19fe279e418104daa79a1101b4acbf61abb4a1
Yakkodash <al.milos@mail.ru>
2018-01-14 22:56:56 +0300

    Update connect script

--- connect.sh
+++ connect.sh
@@ -1,11 +1,36 @@
-#!/bin/sh
+#!/bin/bash --
 
-# $1 - ngrok port
-if [ -z "$1" ]
-then
-    echo "ERROR: please specify ngrok port"
-    exit 1
-fi
+TO_DIR="$HOME/battlestation"
+FROM_DIR="/"
+IP="0.tcp.eu.ngrok.io"
+USER="vagrant"
+HOST="battlestation"
+SSH_FLAGS="-i ./vagrant_key"
+SSHFS_FLAGS=""
 
-chmod 600 vagrant_key
-ssh -i vagrant_key -p $1 vagrant@0.tcp.eu.ngrok.io
+for i in "$@"
+do
+case $i in
+	m|-m|--mount)
+  sudo umount $TO_DIR
+  rmdir $TO_DIR
+  mkdir -p $TO_DIR
+	sudo sshfs -o allow_other -oIdentityFile=$(pwd)/vagrant_key $SSHFS_FLAGS $USER@$IP:$FROM_DIR $TO_DIR -o nonempty  -C -p $2
+	if [[ $? -eq 0 ]]; then
+	echo "$HOST mounted"
+	fi
+	;;
+	-d|d|u|-u|--umount)
+	sudo fusermount -u $TO_DIR
+	if [[ $? -eq 0 ]]; then
+	echo "$HOST unmounted"
+	fi
+  rmdir $TO_DIR
+	;;
+*)
+	ssh $SSH_FLAGS -p $1 $USER@$IP
+	;;
+esac
+exit 0
+done
+ssh $SSH_FLAGS -p $1 $USER@$IP
